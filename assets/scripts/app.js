1e5585c70b68ac706b1b8c2e2845cb9b129474cc
Erasmus Antwi <refugedesigns17@gmail.com>
2022-02-20 03:15:58 +0000

    Starting Code - JavaScript Complete Guide

---
+++ assets/scripts/app.js
@@ -0,0 +1,8 @@
+const defaultValue = 0;
+let currentResult = defaultValue;
+
+currentResult = ((currentResult + 10) * 3) / 2 - 1;
+
+let calculationDescription = `( ${defaultValue} + 10 ) * 3 / 2 - 1`;
+
+outputResult(currentResult, calculationDescription);
