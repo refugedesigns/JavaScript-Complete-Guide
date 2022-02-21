ba2596e354d6a8059ee7981fc55cd7711dca13fc
Erasmus Antwi <refugedesigns17@gmail.com>
2022-02-21 10:00:06 +0000

    completed javascript basis

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,59 @@
 const defaultValue = 0;
 let currentResult = defaultValue;
+const logEntries = [];
 
-function add() {
-  const enteredNumber = +userInput.value;
-  const calcDescription = `${currentResult} + ${enteredNumber}`;
-  currentResult = currentResult + enteredNumber;
+function getUserInputNum() {
+  return parseInt(userInput.value);
+}
+
+function createAndWriteOutput(operator, initialResult, currentNum) {
+  const calcDescription = `${initialResult} ${operator} ${currentNum}`;
   outputResult(currentResult, calcDescription);
 }
 
+function writeToLog(opIdentifier, prevResult, opNumber, newResult) {
+  const logEntry = {
+    opIdentifier,
+    prevResult,
+    opNumber,
+    newResult,
+  };
+  logEntries.push(logEntry);
+}
+
+function add() {
+  const enteredNumber = getUserInputNum();
+  const initialResult = currentResult;
+  currentResult = currentResult + enteredNumber;
+  createAndWriteOutput("+", initialResult, enteredNumber);
+  writeToLog("ADD", initialResult, enteredNumber, currentResult);
+}
+
+function subtract() {
+  const enteredNumber = getUserInputNum();
+  const initialResult = currentResult;
+  currentResult = currentResult - enteredNumber;
+  createAndWriteOutput("-", initialResult, enteredNumber);
+  writeToLog("SUBTRACT", initialResult, enteredNumber, currentResult);
+}
+
+function multiply() {
+  const enteredNumber = getUserInputNum();
+  const initialResult = currentResult;
+  currentResult = currentResult * enteredNumber;
+  createAndWriteOutput("x", initialResult, enteredNumber);
+  writeToLog("MULTIPLY", initialResult, enteredNumber, currentResult);
+}
+
+function divide() {
+  const enteredNumber = getUserInputNum();
+  const initialResult = currentResult;
+  currentResult = currentResult / enteredNumber;
+  createAndWriteOutput("/", initialResult, enteredNumber);
+  writeToLog("DIVIDE", initialResult, enteredNumber, currentResult);
+}
+
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
 addBtn.addEventListener("click", add);
